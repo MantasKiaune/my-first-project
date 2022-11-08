8c8baa85b1f6a38d1071812dcf50c23b8ab0781a
MantasKiaune <117673534+MantasKiaune@users.noreply.github.com>
2022-11-08 22:02:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 #include <cstddef>
 #include <cstring>
 using namespace std;
+
+class studentas{
+    private:
+    string vardas;
+    string pavarde;
+    int namudarbai[];
+    int egzaminas;
+    double vidurkis;
+    public:
+    void output{
+        cout << "Iveskite varda" << endl;
+        cin >> vardas;
+        cout << "Iveskite pavarde" << endl;
+        cin >> pavarde;
+        cout << "Iveskite namu darbu kieki" << endl;
+        cin >> namudarbai;
+        cout << "Iveskite egzamino rezultata" << endl;
+        cin >> egzaminas;
+    }
 int main(){
 
 
